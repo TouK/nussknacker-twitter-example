4c1b342d7e0ec772ae48a8353c9821d53a39ab1a
Arek Burdach <arek.burdach@gmail.com>
2022-09-18 22:12:21 +0200

    copied twitter connector

---
+++ twitter-connector/src/main/java/com/github/jcustenborder/kafka/connect/twitter/TwitterSourceConnectorConfig.java
@@ -0,0 +1,90 @@
+/**
+ * Copyright © 2016 Jeremy Custenborder (jcustenborder@gmail.com)
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.jcustenborder.kafka.connect.twitter;
+
+import com.github.jcustenborder.kafka.connect.utils.config.ConfigKeyBuilder;
+import org.apache.kafka.common.config.AbstractConfig;
+import org.apache.kafka.common.config.ConfigDef;
+import org.apache.kafka.common.config.ConfigDef.Importance;
+import org.apache.kafka.common.config.ConfigDef.Type;
+
+import java.util.Map;
+import java.util.Set;
+
+
+public class TwitterSourceConnectorConfig extends AbstractConfig {
+
+  public static final String TWITTER_DEBUG_CONF = "twitter.debug";
+  public static final String TWITTER_BEARER_TOKEN_CONF = "twitter.bearerToken";
+  public static final String FILTER_RULE_CONF = "filter.rule";
+  public static final String KAFKA_STATUS_TOPIC_CONF = "kafka.status.topic";
+  public static final String KAFKA_STATUS_TOPIC_DOC = "Kafka topic to write the statuses to.";
+  public static final String PROCESS_DELETES_CONF = "process.deletes";
+  public static final String PROCESS_DELETES_DOC = "Should this connector process deletes.";
+  public static final String QUEUE_EMPTY_MS_CONF = "queue.empty.ms";
+  public static final String QUEUE_BATCH_SIZE_CONF = "queue.batch.size";
+  private static final String TWITTER_DEBUG_DOC = "Flag to enable debug logging for the twitter api.";
+  private static final String TWITTER_BEARER_TOKEN_DOC = "Bearer token";
+  private static final String FILTER_RULE_DOC = "Twitter rule used in filtering.";
+  public static final String QUEUE_EMPTY_MS_DOC = "The amount of time to wait if there are no records in the queue.";
+  public static final String QUEUE_BATCH_SIZE_DOC = "The number of records to return in a single batch.";
+
+
+  public final String topic;
+  public final boolean twitterDebug;
+  public final boolean processDeletes;
+  public final String filterRule;
+  public final int queueEmptyMs;
+  public final int queueBatchSize;
+  public final String bearerToken;
+
+  public TwitterSourceConnectorConfig(Map<String, String> parsedConfig) {
+    super(conf(), parsedConfig);
+    this.topic = this.getString(KAFKA_STATUS_TOPIC_CONF);
+    this.twitterDebug = this.getBoolean(TWITTER_DEBUG_CONF);
+    this.processDeletes = this.getBoolean(PROCESS_DELETES_CONF);
+    this.filterRule = getString(FILTER_RULE_CONF);
+    this.queueBatchSize = getInt(QUEUE_BATCH_SIZE_CONF);
+    this.queueEmptyMs = getInt(QUEUE_EMPTY_MS_CONF);
+    this.bearerToken = getString(TWITTER_BEARER_TOKEN_CONF);
+  }
+
+  public static ConfigDef conf() {
+    return new ConfigDef()
+        .define(TWITTER_DEBUG_CONF, Type.BOOLEAN, false, Importance.LOW, TWITTER_DEBUG_DOC)
+        .define(TWITTER_BEARER_TOKEN_CONF, Type.PASSWORD, Importance.HIGH, TWITTER_BEARER_TOKEN_DOC)
+        .define(FILTER_RULE_CONF, Type.STRING, Importance.HIGH, FILTER_RULE_DOC)
+        .define(KAFKA_STATUS_TOPIC_CONF, Type.STRING, Importance.HIGH, KAFKA_STATUS_TOPIC_DOC)
+        .define(PROCESS_DELETES_CONF, Type.BOOLEAN, Importance.HIGH, PROCESS_DELETES_DOC)
+        .define(
+            ConfigKeyBuilder.of(QUEUE_EMPTY_MS_CONF, Type.INT)
+                .importance(Importance.LOW)
+                .documentation(QUEUE_EMPTY_MS_DOC)
+                .defaultValue(100)
+                .validator(ConfigDef.Range.atLeast(10))
+                .build()
+        )
+        .define(
+            ConfigKeyBuilder.of(QUEUE_BATCH_SIZE_CONF, Type.INT)
+                .importance(Importance.LOW)
+                .documentation(QUEUE_BATCH_SIZE_DOC)
+                .defaultValue(100)
+                .validator(ConfigDef.Range.atLeast(1))
+                .build()
+        );
+  }
+
+}
